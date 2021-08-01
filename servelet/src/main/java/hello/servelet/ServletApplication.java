68d92e2c9467c32d40778430e272cfd4388ae411
developingbear <developingbear@gmail.com>
2021-08-01 13:36:14 +0900

    class: Servelet - HelloServlet

--- servelet/src/main/java/hello/servelet/ServletApplication.java
+++ servelet/src/main/java/hello/servelet/ServletApplication.java
@@ -2,12 +2,14 @@ package hello.servelet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class ServeletApplication {
+@ServletComponentScan
+public class ServletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServeletApplication.class, args);
+		SpringApplication.run(ServletApplication.class, args);
 	}
 
 }
