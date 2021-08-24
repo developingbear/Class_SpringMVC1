98d761c50a654821cfe16cd290ecfeda7b0697d9
developingbear <developingbear@gmail.com>
2021-08-24 13:46:26 +0900

    class: Spring MVC : Item Detail

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -19,6 +20,20 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 1000, 10));
+        itemRepository.save(new Item("itemB", 500, 5));
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     @GetMapping
     public String items(Model model) {
         List<Item> all = itemRepository.findAll();
@@ -26,9 +41,4 @@ public class BasicItemController {
         return "basic/items";
     }
 
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 1000, 10));
-        itemRepository.save(new Item("itemB", 500, 5));
-    }
 }
